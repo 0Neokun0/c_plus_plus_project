799230ff0bd997cb453ec54f5d989b0552e06147
0Neokun0 <nishant.meher.9955@gmail.com>
2023-08-28 15:49:23 +0900

    8/28_Prev.Prevとデートフォーマットレビジョン2桁

--- Files to Update/DasExdpLoadDataVersionInfo.h
+++ Files to Update/DasExdpLoadDataVersionInfo.h
@@ -29,8 +29,10 @@
 // DasCLoadDataÇ÷ÇÃïœçXëŒâû
 //const Vi_Version DasExdpLoadData_Version_Prev ("DasExdpLoadData.Prev    ", "2013/12/06", "554", "01", "RELS_DAS_554_01", "", "hp");
 //const Vi_Version DasExdpLoadData_Version ("DasExdpLoadData", "2014/06/05", "556", "01", "RELS_DASEXDPLOADDATA_556-01", "", "hp");
-const Vi_Version DasExdpLoadData_Version_Prev ("DasExdpLoadData", "2014/06/05", "556", "01", "RELS_DASEXDPLOADDATA_556-01", "", "hp");
+// const Vi_Version DasExdpLoadData_Version_Prev ("DasExdpLoadData", "2014/06/05", "556", "01", "RELS_DASEXDPLOADDATA_556-01", "", "hp");
 //const Vi_Version DasCLoadData_Version       ("DasCLoadData",    "2021/12/05", "000", "01", "RELS_DASCLOADDATA_000-01", "", "hp");
-const Vi_Version DasCLoadData_Version         ("DasCLoadData",    "2023/03/28", "000", "02", "RELS_DASCLOADDATA_000-02", "", "dxc");
+//const Vi_Version DasCLoadData_Version         ("DasCLoadData",    "2023/03/28", "000", "02", "RELS_DASCLOADDATA_000-02", "", "dxc");
+const Vi_Version DasCLoadData_Version _Prev("DasCLoadData.Prev", "2023/08/28", "000", "03", "RELS_DASCLOADDATA_000-03", "", "dxc");
+const Vi_Version DasCLoadData_Version ("DasCLoadData", "2023/08/28", "001", "001", "RELS_DASCLOADDATA_001-01", "", "dxc");
 
 #endif
